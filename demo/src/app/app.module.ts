e14af3439dd1597e7f90174e1ea98ae9f0075ce3
Viliam Rockai <viliam.rockai@gmail.com>
2019-03-06 16:51:23 +0100

    Fix demo

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
-import { QueryBuilderModule } from '../../lib';
+import { QueryBuilderModule } from 'angular2-query-builder';
 
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import {
